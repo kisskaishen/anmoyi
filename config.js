2a515ab394a3b5b9e4e72d199e2cdd000e3ae07b
kisskaishen <kisskaishen@163.com>
2018-09-20 13:58:18 +0800

    修改样式

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 let Config = {
-  Host:'http://www.nuojia.com/small/',
+  Host: 'http://120.79.172.145/small/',
+  // Host:'http://www.nuojia.com/small/',
   Mapkey: 'PPKBZ-IG33X-NEU45-7BWGM-BELQJ-KDBEU'
 }
 
--- pages/user/user.js
+++ pages/user/user.js
@@ -1,33 +1,68 @@
-
 const App = getApp();
 Page({
-  data: {
-    footer: {
-      active: "usercenter"
+    data: {
+        closeImg: false, // 清除图标隐藏
+        tel: '', // 输入手机号
+        captcha: '', // 验证码
+        isFocus:true,   // 获取焦点
+        footer: {
+            active: "usercenter"
+        },
+        isLogin: false
     },
-    isLogin:false
-  },
-  onLoad: function (options) {
-    wx.setNavigationBarTitle({
-      title: '个人中心'
-    })
-  },
-  onReady: function () {
+    onLoad: function(options) {
+        wx.setNavigationBarTitle({
+            title: '个人中心'
+        })
+    },
+    onReady: function() {
 
-  },
-  onShow: function () {
-    
-  },
-  onHide: function () {
-  
-  },
-  onUnload: function () {
-  
-  },
-  dologin:function(){
-    wx.navigateTo({
-      url: '/pages/login/login',
-    })
-  }
+    },
+    onShow: function() {
+
+    },
+    onHide: function() {
+
+    },
+    onUnload: function() {
+
+    },
+    dologin: function() {
+        wx.navigateTo({
+            url: '/pages/login/login',
+        })
+    },
+    // 获取手机号的值
+    getTelBind(e) {
+        let that = this;
+        that.setData({
+            tel: e.detail.value,
+            closeImg: true
+        })
+    },
+    // 清除手机号
+    clearTel() {
+        let that = this;
+        that.setData({
+            tel: '',
+            closeImg: false
+        })
+    },
+    // 验证码
+    Focus(e) {
+        let that = this;
+        let inputValue = e.detail.value;
+        that.setData({
+            captcha: inputValue,
+        })
+    },
+    Tap() {
+        let that = this;
+        that.setData({
+            isFocus: true,
+        })
+    }, 
+    // 立即绑定
+    bindTel() {}
 
 })
